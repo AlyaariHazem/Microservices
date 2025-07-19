ccc14e38f261f142cf87caa5f8a87c7f29f31216
Hazem Alyaari <133292426+AlyaariHazem@users.noreply.github.com>
2025-07-19 23:45:08 +0300

    Add Get Products to get All the Products

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,21 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿
+namespace Catalog.API.Products.GetProducts
 {
-    public class GetProductsEndpoint
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var respone = result.Adapt<GetProductsResponse>();
+                return Results.Ok(respone);
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+                .WithSummary("Get Products")
+                .WithDescription("Get Products");
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -3,6 +3,14 @@
     public record GetProductsQuery():IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> products);
     internal class GetProductsQueryHandler
+        (IDocumentSession session,ILogger<GetProductsQueryHandler> logger)
+        :IQueryHandler<GetProductsQuery,GetProductsResult>
     {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Get");
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
     }
 }
