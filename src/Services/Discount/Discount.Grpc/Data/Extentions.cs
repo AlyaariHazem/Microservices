14b8153a02b99108403352cc8658a671a956edb5
Hazem Alyaari <133292426+AlyaariHazem@users.noreply.github.com>
2025-08-11 23:29:22 +0300

    Add Discount and Discount.Grpc projects to solution
    - Updated `Microservices.sln` to include new projects for "Discount" and "Discount.Grpc".
    - Modified `appsettings.Development.json` and `appsettings.json` for logging and SQLite connection strings.
    - Created `discount.db` with "Coupons" and "__EFMigrationsHistory" tables and initial data.
    - Added `Discount.Grpc.csproj` with project settings and dependencies.
    - Created `Dockerfile` for building and running the Discount.Grpc service.
    - Updated `Program.cs` to configure gRPC service and SQLite context.
    - Introduced `DiscountContext.cs` for managing "Coupons" with data seeding.
    - Added `Extentions.cs` for middleware to apply database migrations.
    - Generated migration files for managing the "Coupons" table schema.
    - Created `Coupon.cs` model class for coupon entity representation.
    - Updated `launchSettings.json` for application profiles, including Docker.
    - Added `discounts.proto` for defining gRPC service and messages.
    - Implemented `DiscountService.cs` for CRUD operations on discounts.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigrations(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            context.Database.MigrateAsync();
+            return app;
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/discountService.cs
@@ -0,0 +1,46 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService 
+        (DiscountContext dbcontext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbcontext.Coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+            if (coupon is null)
+                coupon = new Coupon{ProductName = "No Discount", Description = "No Discount", Amount = 0 };
+            logger.LogInformation("Discount is retrieved for ProductName : {ProductName}, Amount : {Amount}", 
+                coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+
+        }
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid coupon data"));
+            dbcontext.Coupons.Add(coupon);
+            await dbcontext.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully created for ProductName : {ProductName}, Amount : {Amount}", 
+                coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
